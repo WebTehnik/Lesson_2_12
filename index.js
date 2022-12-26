45eec995f5b5b8c89f49b396c82ca19348d08974
Yuri-PC\Dell5570 <webtehnik@gmail.com>
2022-12-26 21:51:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// JavaScript-код с созданием переменных с помощью способа //
+
+/*
+Автор
+Юрий Чех
+*/
+
+const myName = 'Юрий';
+const mySurname = 'Чех';
+
+let myFavoriteDrink, myFavoriteAnimal, myFavoriteProgrammingLanguage;
+
+myFavoriteDrink = 'Молоко';
+myFavoriteAnimal = 'Мой верный пёс Марсик';
+myFavoriteProgrammingLanguage = 'Надеюсь, что скоро это будет JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
